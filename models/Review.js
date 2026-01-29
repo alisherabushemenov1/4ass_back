fe6091a3c1740d55297cea417d0d684e947eff38
alisherabushemenov1 <alisher.abushemenov1@gmail.com>
2026-01-29 20:23:00 +0500

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,52 +1,44 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: [true, 'Product ID is required']
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: [true, 'Product ID is required'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'Rating is required'],
+      min: [1, 'Rating must be at least 1'],
+      max: [5, 'Rating cannot exceed 5'],
+      validate: {
+        validator: Number.isInteger,
+        message: 'Rating must be an integer between 1 and 5',
+      },
+    },
+    comment: {
+      type: String,
+      required: [true, 'Comment is required'],
+      minlength: [5, 'Comment must be at least 5 characters long'],
+      maxlength: [1000, 'Comment cannot exceed 1000 characters'],
+      trim: true,
+    },
+    recommended: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User is required'], 
+    },
   },
-  author: {
-    type: String,
-    required: [true, 'Author name is required'],
-    trim: true,
-    minlength: [2, 'Author name must be at least 2 characters long'],
-    maxlength: [50, 'Author name cannot exceed 50 characters']
-  },
-  rating: {
-    type: Number,
-    required: [true, 'Rating is required'],
-    min: [1, 'Rating must be at least 1'],
-    max: [5, 'Rating cannot exceed 5'],
-    validate: {
-      validator: Number.isInteger,
-      message: 'Rating must be an integer between 1 and 5'
-    }
-  },
-  comment: {
-    type: String,
-    required: [true, 'Comment is required'],
-    minlength: [5, 'Comment must be at least 5 characters long'],
-    maxlength: [1000, 'Comment cannot exceed 1000 characters']
-  },
-  recommended: {
-    type: Boolean,
-    default: true
-  },
-  // Reference to user who created the review
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
-}, {
-  timestamps: true
-});
+  { timestamps: true }
+);
 
-// Indexes
 reviewSchema.index({ productId: 1 });
 reviewSchema.index({ rating: -1 });
 reviewSchema.index({ createdBy: 1 });
 
-const Review = mongoose.model('Review', reviewSchema);
-
-module.exports = Review;
+module.exports = mongoose.model('Review', reviewSchema);
